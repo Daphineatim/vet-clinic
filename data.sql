9c777efc802e94f115626a69f67cc39c0db5f496
Daphineatim <hopedaphine19@gmail.com>
2022-12-14 17:51:41 +0300

    Add data into table in data.sql

--- data.sql
+++ data.sql
@@ -3,4 +3,11 @@
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumon', '2020-02-03', 0, true, 10.23 ),
 ('Gabumon', '2018-11-15', 2, true, 8),
 ('Pikachu', '2021-01-07', 1, false, 15.04),
-('Devimon', '2017-05-12', 5, true, 11);
+('Devimon', '2017-05-12', 5, true, 11),
+('Charmander', '2020-02-08', 0, false, -11),
+('Plantmon', '12-11-2021', 2, true, -5.7);
+('Squirtle', '02-04-1993', 3, false, -12.13);
+('Angemon', '12-06-2005', 1, true, -45);
+('Boarmon', '07-06-2005', 7, true, 20.4);
+('Blossom', '13-11-1998', 3, true, 17);
+('Ditto', '14-05-2022', 4, true, 22);
