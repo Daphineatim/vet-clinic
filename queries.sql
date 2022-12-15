81b95695068d232a45f66a1ded033715a66d9ee8
Daphineatim <hopedaphine19@gmail.com>
2022-12-15 20:21:17 +0300

    Modify data.sql, queries.sql, schema.sql

--- queries.sql
+++ queries.sql
@@ -9,6 +9,8 @@ SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg>=10.4 AND weight_kg<=17.3;
 
+-- SECOND PROJECT
+
 BEGIN;
 UPDATE animals SET species = 'undefined';
 ROLLBACK;
@@ -37,7 +39,7 @@ SELECT name FROM animals WHERE escape_attempts = (SELECT MAX(escape_attempts) FR
 SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth>'01-01-1990' AND date_of_birth<'01-01-2000' GROUP BY species;
 
--- Third project
+-- THIRD PROJECT
 
 -- What animals belong to Melody Pond?
 SELECT name FROM animals JOIN owners
@@ -74,3 +76,23 @@ SELECT owners.full_name as full_name , COUNT(*) as count FROM animals
 JOIN owners ON animals.owner_id = owners.id 
 GROUP BY owners.full_name 
 ORDER BY count DESC LIMIT 1;
+
+-- FOURTH PROJECT 
+
+SELECT animals.name FROM animals JOIN visits ON animals.id = visits.animals_id JOIN vets ON visits.vets_id = vets.id WHERE vets.name='William Tatcher' ORDER BY visits.visit_date DESC LIMIT 1;
+
+SELECT COUNT(*) AS DifferentAnimalsCount FROM (SELECT DISTINCT animals.name FROM animals JOIN visits ON animals.id = visits.animals_id JOIN vets ON visits.vets_id = vets.id WHERE vets.name='Stephanie Mendez') AS foo;
+
+SELECT vets.name, species.name FROM vets FULL JOIN specializations ON vets.id = specializations.vets_id FULL JOIN species ON specializations.species_id = species.id;
+
+SELECT animals.name FROM animals JOIN visits ON animals.id = visits.animals_id JOIN vets ON visits.vets_id = vets.id WHERE vets.name='Stephanie Mendez' AND visits.visit_date > '2019-04-01' AND visits.visit_date <'2020-08-30';
+
+SELECT animals.name, COUNT(*) FROM animals JOIN visits ON animals.id = visits.animals_id JOIN vets ON visits.vets_id = vets.id GROUP BY animals.name ORDER BY count DESC LIMIT 1;
+
+SELECT animals.name FROM animals JOIN visits ON animals.id = visits.animals_id JOIN vets ON visits.vets_id = vets.id WHERE vets.name='Maisy Smith' ORDER BY visits.visit_date ASC LIMIT 1;
+
+SELECT animals.*, vets.*, visits.visit_date FROM animals JOIN visits ON animals.id = visits.animals_id JOIN vets ON visits.vets_id = vets.id ORDER BY visits.visit_date DESC LIMIT 1;
+
+SELECT COUNT(*) FROM specializations RIGHT JOIN vets ON specializations.vets_id = vets.id JOIN visits ON vets.id = visits.vets_id  JOIN animals ON animals.id = visits.animals_id JOIN species ON animals.species_id = species.id WHERE specializations.species_id <> species.id OR specializations.species_id IS NULL;
+
+SELECT species.name, COUNT(*) FROM vets JOIN visits ON vets.id = visits.vets_id JOIN animals ON animals.id = visits.animals_id JOIN species ON animals.species_id = species.id WHERE vets.name='Maisy Smith' GROUP BY species.name ORDER BY count DESC LIMIT 1;
--- schema.sql
+++ schema.sql
@@ -10,12 +10,12 @@ CREATE TABLE animals
     weight_kg decimal
 );
 
--- PROJECT 2
+--SECOND PROJECT 
 
 ALTER TABLE animals
 ADD species varchar;
 
--- PROJECT 3
+--THIRD PROJECT 
 
 CREATE TABLE owners (
     id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
@@ -33,3 +33,27 @@ ALTER TABLE animals ADD COLUMN species_id int;
 ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species (id);
 ALTER TABLE animals ADD COLUMN owner_id int;
 ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners (id);
+
+-- FOURTH PROJECT
+
+CREATE TABLE vets (
+    id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name varchar,
+    age int,
+    date_of_graduation date
+);
+
+CREATE TABLE specializations (
+    species_id int REFERENCES species (id),
+    vets_id int REFERENCES vets (id),
+    PRIMARY KEY (species_id, vets_id)
+);
+
+ALTER TABLE animals ADD PRIMARY KEY (id);
+
+CREATE TABLE visits (
+    animals_id int REFERENCES animals (id),
+    vets_id int REFERENCES vets (id),
+    visit_date date,
+    PRIMARY KEY (animals_id, vets_id, visit_date)
+);
